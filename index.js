f36a4c26610459b8a063a424f039206658dee768
Václav <vaclav.slik@gmail.com>
2026-02-25 16:58:23 +0100

    Initial Node project setup

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is ready!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
